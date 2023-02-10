ca683a7e109c623fa98f5fb5840778d8a00cb68c
kevinnz-defend <123701774+kevinnz-defend@users.noreply.github.com>
2023-02-10 16:34:35 +1300

    added extra files

--- HiddenService/__main__.py
+++ HiddenService/__main__.py
@@ -1,13 +1,10 @@
 from flask import Flask
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return "<h1>I am suppose to be hidden</h1>"
-
 
 if __name__ == "__main__":
     app.run(port=3001)
---
+++ start_hidden.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 "./HiddenService/__main__.py" &
